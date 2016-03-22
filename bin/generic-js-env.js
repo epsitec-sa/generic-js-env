d248adaa9174ae89f7686be1a11ea28d6790ab97
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-22 18:27:29 +0100

    Use the right line terminator accordingly to the platform

--- bin/generic-js-env.js
+++ bin/generic-js-env.js
@@ -11,8 +11,16 @@ var suffix = '/node_modules/generic-js-env';
 if (cwd.endsWith (suffix)) {
   var root = cwd.substr (0, cwd.length - suffix.length);
   var files = fs.readdirSync (path.join (cwd, 'templates'));
+
+  var regex = /\r\n/g;
+  var newline = '\n';
+  if (process.platform === 'win32') {
+    regex = /[^\r]\n/g;
+    newline = '\r\n';
+  }
+
   files.forEach (function (file) {
     var data = fs.readFileSync (path.join (cwd, 'templates', file));
-    fs.writeFileSync (path.join (root, file), data);
+    fs.writeFileSync (path.join (root, file), data.replace (regex, newline));
   });
 }
